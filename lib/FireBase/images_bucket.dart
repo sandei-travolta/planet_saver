d250e6642c64313ecd5b27032f80b3f6ca3e057f
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-06-16 15:47:05 +0300

    Fixed the issue on fetching products

--- lib/FireBase/images_bucket.dart
+++ lib/FireBase/images_bucket.dart
@@ -5,7 +5,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 class ImagesBucket{
 Future<String?> uploadImage(File image,String id)async{
   try{
-    Reference reference=FirebaseStorage.instance.ref().child("ProfileImages").child(id);
+    Reference reference=FirebaseStorage.instance.ref().child("ProfileImages").child(image.path);
     UploadTask uploadTask=reference.putFile(image);
     TaskSnapshot snapshot=await uploadTask;
     return await snapshot.ref.getDownloadURL();
