6894076b5045fde303597801967391d01b122386
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-07-17 10:44:30 +0300

    goood morning

---
+++ lib/Models/TransactionModel.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class TransactionModel{
+  final String date;
+  final int amount;
+  final String senderID;
+  final String receiverID;
+  final String status;
+
+  TransactionModel({required this.date, required this.amount, required this.senderID, required this.receiverID,required this.status});
+  Map<String,dynamic> toJson()=>{
+    "Date":date,
+    "Amount":amount,
+    "SenderID":senderID,
+    "ReceiverID":receiverID,
+    "Status":status
+  };
+  static TransactionModel fromSnap(DocumentSnapshot documentSnapshot){
+    var snapshot =documentSnapshot.data() as Map<String,dynamic>;
+    return TransactionModel(
+        date: snapshot['Date'],
+        amount: snapshot['Amount'],
+        senderID: snapshot['SenderID'],
+        receiverID: snapshot['ReceiverID'],
+        status: snapshot['Status']
+    );
+  }
+}
---
+++ lib/Models/orderModel.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class OrderModel{
+  final String orderTittle;
+  final String orderPrice;
+  final String orderId;
+  final String orderDate;
+  final String datePlaced;
+  final String buyerId;
+  final String sellerId;
+  final bool status;
+
+  OrderModel({required this.orderTittle,required this.orderPrice,required this.orderId, required this.orderDate, required this.datePlaced,required this.buyerId, required this.sellerId,required this.status});
+
+  Map<String,dynamic> toJson()=>{
+    "OrderTittle":orderTittle,
+    "OrderPrice":orderPrice,
+    "OrderID":orderId,
+    "OrderDate":orderDate,
+    "DatePlaced":datePlaced,
+    "BuyerId":buyerId,
+    "SellerId":sellerId,
+    "Status":status
+  };
+  static OrderModel fromSnap(DocumentSnapshot documentSnapshot){
+    var snapShot=documentSnapshot.data() as Map<String,dynamic>;
+    return OrderModel(
+        orderTittle:snapShot['OrderTittle'],
+        orderPrice: snapShot['OrderPrice'],
+        orderId: snapShot['OrderId'],
+        orderDate: snapShot['OrderDate'],
+        datePlaced: snapShot['DatePlaced'],
+        buyerId:snapShot['BuyerId'],
+        sellerId: snapShot['sellerId'],
+        status: snapShot['status']
+    );
+  }
+}
