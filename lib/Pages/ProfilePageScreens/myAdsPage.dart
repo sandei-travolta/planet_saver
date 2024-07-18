8e1a6720203b5f3ce2420e7f86ce026549a7c2e0
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-07-18 22:19:19 +0300

    Fetch user posted ads

---
+++ lib/Pages/ProfilePageScreens/myAdsPage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class MyAdsPage extends StatelessWidget {
+  const MyAdsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
