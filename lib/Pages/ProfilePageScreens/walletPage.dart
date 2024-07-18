2ad9a96c724fe8cf8f32e25742c4970042ffe766
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-07-18 22:15:36 +0300

    Fetch user posted ads

---
+++ lib/Pages/ProfilePageScreens/walletPage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class WalletPage extends StatelessWidget {
+  const WalletPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
