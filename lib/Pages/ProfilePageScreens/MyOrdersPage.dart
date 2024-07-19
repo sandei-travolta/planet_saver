16424e8282deba0d1fb4e84cdb3a0007dac4a8d8
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-07-19 22:00:42 +0300

    created the users orders page

--- lib/Pages/ProfilePageScreens/MyOrdersPage.dart
+++ lib/Pages/ProfilePageScreens/MyOrdersPage.dart
@@ -17,42 +17,57 @@ class _MyOrdersPageState extends State<MyOrdersPage> {
   OrdersFireBase ordersFireBase=OrdersFireBase();
   List<OrderModel> orders=[];
   final user=Get.find<UserStateController>();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchOrders();
+  }
   void fetchOrders()async{
     orders=await ordersFireBase.getOrders(user.currentser.value!.uid);
     setState(() {});
   }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-            flex: 2,
-            child: Container(
-              color: primaryColorV2.withOpacity(0.8),
-              child: Center(
-                child:Text("My Orders",style: TextStyle(fontSize: 30,fontFamily: "PoppinsBold"),),
-              ),
-            )
-        ),
-        Expanded(
-            flex: 11,
-            child: Container(
-              child: orders.isEmpty
-                  ? Center(child:Text("You Have No Orders",
-                  style: TextStyle(
-                    fontSize: 18,
-                  )))
-                  :ListView.builder(
-                  itemCount: orders.length,
-                  itemBuilder: (context,index){
-                    OrderModel orderModel=orders[index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5.0,vertical: 5),
-                      child: OrdersCard(orderModel: orderModel,currentUser: user.currentser.value),
-                    );
-                  }),
-            ))
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              flex: 2,
+              child: SafeArea(
+                child: Container(
+                  color: primaryColorV2.withOpacity(0.8),
+                  width: double.maxFinite,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("My Orders",style: TextStyle(fontSize: 30,fontFamily: "PoppinsBold"),),
+                    ],
+                  ),
+                ),
+              )
+          ),
+          Expanded(
+              flex: 11,
+              child: Container(
+                child: orders.isEmpty
+                    ? Center(child:Text("You Have No Orders",
+                    style: TextStyle(
+                      fontSize: 18,
+                    )))
+                    :ListView.builder(
+                    itemCount: orders.length,
+                    itemBuilder: (context,index){
+                      OrderModel orderModel=orders[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0,vertical: 5),
+                        child: OrdersCard(orderModel: orderModel,currentUser: user.currentser.value),
+                      );
+                    }),
+              ))
+        ],
+      ),
     );
   }
 }
