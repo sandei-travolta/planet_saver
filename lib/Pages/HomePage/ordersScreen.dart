547fc0c55b4e2f26410a39ef377c6ccc469b0f59
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-07-19 10:29:37 +0300

    changes to orders page

--- lib/Pages/HomePage/ordersScreen.dart
+++ lib/Pages/HomePage/ordersScreen.dart
@@ -45,11 +45,65 @@ class OrdersScreen extends StatelessWidget {
         Expanded(
             flex: 11,
             child: Container(
-              child: Text("No orders set for ${formattedDate}",
+              /*child: Text("No orders set for ${formattedDate}",
                 style: TextStyle(
                   fontSize: 18,
                 ),
-              ),
+              ),*/
+              child: ListView.builder(
+                  itemCount: 3,
+                  itemBuilder: (ontext,index){
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0,vertical: 5),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    height: 120,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.3)
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text("Order-Tittle",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w800),)
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Order-price",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),),
+                            Text("Order-Date",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),)
+                          ],
+                        ),
+                        const SizedBox(height: 3,),
+                        Row(
+                          children: [
+                            Text("Seller",style: TextStyle(fontSize: 15,fontWeight: FontWeight.w800),)
+                          ],
+                        ),
+                        const SizedBox(height: 15,),
+                        Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: primaryColorV1
+                              ),
+                              child: Center(
+                                child: Text("Marks Order Complete",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white
+                                ),)
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                );
+              }),
             ))
       ],
     );
